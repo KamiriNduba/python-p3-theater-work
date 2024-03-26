c793e28044aabbd4ba2969a536fc62a09f44abda
NDUBA <user@users-MacBook-Pro.local>
2024-03-26 14:13:31 +0300

    done

--- lib/models.py
+++ lib/models.py
@@ -8,3 +8,50 @@ convention = {
 metadata = MetaData(naming_convention=convention)
 
 Base = declarative_base(metadata=metadata)
+
+class Role(Base):
+    __tablename__ = 'roles'
+
+    id = Column(Integer, primary_key=True)
+    character_name = Column(String)
+
+    # Define one-to-many relationship with Audition
+    auditions = relationship("Audition", back_populates="role")
+
+    def actors(self):
+        return [audition.actor for audition in self.auditions]
+
+    def locations(self):
+        return [audition.location for audition in self.auditions]
+
+    def lead(self):
+        hired_auditions = [audition for audition in self.auditions if audition.hired]
+        if hired_auditions:
+            return hired_auditions[0]
+        else:
+            return 'no actor has been hired for this role'
+
+    def understudy(self):
+        hired_auditions = [audition for audition in self.auditions if audition.hired]
+        if len(hired_auditions) > 1:
+            return hired_auditions[1]
+        else:
+            return 'no actor has been hired for understudy for this role'
+
+class Audition(Base):
+    __tablename__ = 'auditions'
+
+    id = Column(Integer, primary_key=True)
+    actor = Column(String)
+    location = Column(String)
+    hired = Column(Boolean, default=False)
+    role_id = Column(Integer, ForeignKey('roles.id'))
+
+    # Define many-to-one relationship with Role
+    role = relationship("Role", back_populates="auditions")
+
+    def call_back(self):
+        self.hired = True
+
+# Create the tables
+Base.metadata.create_all(engine)
